852fae10d08885e31e1e3e54439ac465fb7301bb
Austin Christensen <Austin@MacBook-Pro-2.local>
2017-07-20 20:23:32 -0500

    first

---
+++ index.js
@@ -0,0 +1,4 @@
+var express = require('express');  // framework
+var routes = require('./routes');   // where routes will be defined
+var http = require('http');         // protocol 
+var path = require('path');  	// node module to resolve paths
